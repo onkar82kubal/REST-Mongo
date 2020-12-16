e292663db1b8b0edaf627cdf6edacd9ff9dc004c
Onkar Kubal <onkar82kubal@gmail.com>
2020-12-16 22:35:47 +0530

    Second Commit

---
+++ api/routes/orders.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/',(req,res,next) => {
+	res.status(200).json({
+		message: 'Orders fetched'
+	});
+});
+
+router.post('/',(req,res,next) => {
+	res.status(200).json({
+		message: 'Order was created'
+	});
+});
+
+router.get('/:orderId',(req,res,next) => {
+	res.status(200).json({
+		message: 'Orders details',
+		orderId: req.params.orderId 
+	});
+});
+
+router.delete('/:orderId',(req,res,next) => {
+	res.status(200).json({
+		message: 'Order deleted',
+		orderId: req.params.orderId 
+	});
+});
+
+module.exports = router;
