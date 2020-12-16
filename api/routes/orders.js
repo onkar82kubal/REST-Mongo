bd77e6b8e0eb1813c8bbe981a848c932483ceccf
Onkar Kubal <onkar82kubal@gmail.com>
2020-12-17 02:38:13 +0530

    Eighth commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ const Product = require('../models/product');
 router.get('/',(req,res,next) => {
 	Order.find()
 	.select('product quantity _id')
+	.populate('product','name')
 	.exec()
 	.then(docs => {
 		console.log(docs);
@@ -78,6 +79,7 @@ router.post('/',(req,res,next) => {
 
 router.get('/:orderId',(req,res,next) => {
 	Order.findById(req.params.orderId)
+	.populate('product','name')
 	.exec()
 	.then(order => {
 		res.status(200).json({
