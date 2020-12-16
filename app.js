bab87e922d78697f2846ca5aa7efd24b0ed9fabc
Onkar Kubal <onkar82kubal@gmail.com>
2020-12-16 23:54:46 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
  app.use(morgan('dev'));
+ app.use(bodyParser.urlencoded({extended: false}));
+ app.use(bodyParser.json());
+
+ app.use((req,res,next) => {
+ 	res.header('Access-Control-Allow-Origin','*');
+ 	res.header('Access-Control-Allow-Header',
+ 		'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+ 	if(req.method === 'OPTIONS'){
+ 		res.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+ 		return res.status(200).json({});
+ 	}
+ 	next();
+ });
 
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
