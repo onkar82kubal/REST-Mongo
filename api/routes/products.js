004b3001b36f5cd051683d44174974d53746e26b
Onkar Kubal <onkar82kubal@gmail.com>
2020-12-19 18:56:46 +0530

    JWT Route Protection

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const ObjectId = require('mongodb').ObjectID;
 const moment = require('moment');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+
 const storage = multer.diskStorage({
 	destination: function(req,file,cb){
 		cb(null,'./uploads/');
@@ -64,7 +66,7 @@ router.get('/',(req,res,next) => {
 	});
 });
 
-router.post('/',upload.single('productImage'),(req,res,next) => {	
+router.post('/',checkAuth,upload.single('productImage'),(req,res,next) => {	
 	console.log(req.file);
 	const product = new Product({
 		_id: new mongoose.Types.ObjectId(),
@@ -127,7 +129,7 @@ router.get('/:productId',(req,res,next) => {
 });
 
 
-router.patch('/:productId',(req,res,next) => {
+router.patch('/:productId',checkAuth,(req,res,next) => {
 	const id = req.params.productId;
 	const updateops = {};
 	for(const ops of req.body){
@@ -154,7 +156,7 @@ router.patch('/:productId',(req,res,next) => {
 });
 
 
-router.delete('/:productId',(req,res,next) => {
+router.delete('/:productId',checkAuth,(req,res,next) => {
 	const id = req.params.productId;
 	Product.deleteOne({_id: ObjectId(id)})
 	.exec()
